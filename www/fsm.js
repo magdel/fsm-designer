2a209db1eee80ccee3f6b46c18b486bf656b812e
Pavel <pavelrv@yandex.ru>
2024-09-03 23:45:42 +0300

    updated to current master

--- www/fsm.js
+++ www/fsm.js
@@ -466,6 +466,126 @@ TemporaryLink.prototype.draw = function (c) {
     drawArrow(c, this.to.x, this.to.y, Math.atan2(this.to.y - this.from.y, this.to.x - this.from.x));
 };
 
+// draw using this instead of a canvas and call toJava() afterward
+function ExportAsJava() {
+    this._points = [];
+    this._texData = '';
+    this._scale = 0.1; // to convert pixels to document space (TikZ breaks if the numbers get too big, above 500?)
+    this.strokeStyle = "black";
+
+    this.toJava = function () {
+        return 'Java\n' +
+            '\\usepackage{tikz}\n' +
+            '\n' +
+            '\\begin{document}\n' +
+            '\n' +
+            '\\begin{center}\n' +
+            '\\begin{tikzpicture}[scale=0.2]\n' +
+            '\\tikzstyle{every node}+=[inner sep=0pt]\n' +
+            this._texData +
+            '\\end{tikzpicture}\n' +
+            '\\end{center}\n' +
+            '\n' +
+            '\\end{document}\n';
+    };
+
+    this.beginPath = function () {
+        this._points = [];
+    };
+
+
+    this.arc = function (x, y, radius, startAngle, endAngle, isReversed) {
+        x *= this._scale;
+        y *= this._scale;
+        radius *= this._scale;
+        if (endAngle - startAngle === Math.PI * 2) {
+            this._texData += '\\draw [' + this.strokeStyle + '] (' + fixed(x, 3) + ',' + fixed(-y, 3) + ') circle (' + fixed(radius, 3) + ');\n';
+        } else {
+            if (isReversed) {
+                const temp = startAngle;
+                startAngle = endAngle;
+                endAngle = temp;
+            }
+            if (endAngle < startAngle) {
+                endAngle += Math.PI * 2;
+            }
+            // TikZ needs the angles to be in between -2pi and 2pi or it breaks
+            if (Math.min(startAngle, endAngle) < -2 * Math.PI) {
+                startAngle += 2 * Math.PI;
+                endAngle += 2 * Math.PI;
+            } else if (Math.max(startAngle, endAngle) > 2 * Math.PI) {
+                startAngle -= 2 * Math.PI;
+                endAngle -= 2 * Math.PI;
+            }
+            startAngle = -startAngle;
+            endAngle = -endAngle;
+            this._texData += '\\draw [' + this.strokeStyle + '] (' + fixed(x + radius * Math.cos(startAngle), 3) + ',' + fixed(-y + radius * Math.sin(startAngle), 3) + ') arc (' + fixed(startAngle * 180 / Math.PI, 5) + ':' + fixed(endAngle * 180 / Math.PI, 5) + ':' + fixed(radius, 3) + ');\n';
+        }
+    };
+
+
+    this.moveTo = this.lineTo = function (x, y) {
+        x *= this._scale;
+        y *= this._scale;
+        this._points.push({'x': x, 'y': y});
+    };
+
+
+    this.stroke = function () {
+        if (this._points.length === 0) return;
+        this._texData += '\\draw [' + this.strokeStyle + ']';
+        for (let i = 0; i < this._points.length; i++) {
+            const p = this._points[i];
+            this._texData += (i > 0 ? ' --' : '') + ' (' + fixed(p.x, 2) + ',' + fixed(-p.y, 2) + ')';
+        }
+        this._texData += ';\n';
+    };
+
+
+    this.fill = function () {
+        if (this._points.length === 0) return;
+        this._texData += '\\fill [' + this.strokeStyle + ']';
+        for (let i = 0; i < this._points.length; i++) {
+            const p = this._points[i];
+            this._texData += (i > 0 ? ' --' : '') + ' (' + fixed(p.x, 2) + ',' + fixed(-p.y, 2) + ')';
+        }
+        this._texData += ';\n';
+    };
+
+
+    this.measureText = function (text) {
+        const c = canvas.getContext('2d');
+        c.font = '20px "Times New Romain", serif';
+        return c.measureText(text);
+    };
+
+
+    this.advancedFillText = function (text, originalText, x, y, angleOrNull) {
+        if (text.replace(' ', '').length > 0) {
+            let nodeParams = '';
+            // x and y start off as the center of the text, but will be moved to one side of the box when angleOrNull != null
+            if (angleOrNull != null) {
+                const width = this.measureText(text).width;
+                const dx = Math.cos(angleOrNull);
+                const dy = Math.sin(angleOrNull);
+                if (Math.abs(dx) > Math.abs(dy)) {
+                    if (dx > 0) nodeParams = '[right] ', x -= width / 2;
+                    else nodeParams = '[left] ', x += width / 2;
+                } else {
+                    if (dy > 0) nodeParams = '[below] ', y -= 10;
+                    else nodeParams = '[above] ', y += 10;
+                }
+            }
+            x *= this._scale;
+            y *= this._scale;
+            this._texData += '\\draw (' + fixed(x, 2) + ',' + fixed(-y, 2) + ') node ' + nodeParams + '{$' + originalText.replace(/ /g, '\\mbox{ }') + '$};\n';
+        }
+    };
+
+    this.translate = this.save = this.restore = this.clearRect = function () {
+    };
+}
+
 function ExportAsJson() {
 
 
@@ -1495,6 +1615,16 @@ function saveAsLaTeX() {
     drawUsing(exporter);
     selectedObject = oldSelectedObject;
     let texData = exporter.toLaTeX();
+    copyToClipboard(texData);
+}
+
+function saveAsJava() {
+    let exporter = new ExportAsJava();
+    let oldSelectedObject = selectedObject;
+    selectedObject = null;
+    drawUsing(exporter);
+    selectedObject = oldSelectedObject;
+    let texData = exporter.toJava();
     copyToClipboard(texData);
 }
 
